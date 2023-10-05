bc395b1c50498d10d3c87ec1e6cafec9443606e9
zinoviev <zinovev.ivan@mail.ru>
2023-10-05 23:07:07 +0300

    Все замечания исправлены, только жду ответа от наставника, как вынести размеры в ресурсы

--- app/src/main/java/com/example/playlistmaster/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaster/SettingsActivity.kt
@@ -1,16 +1,13 @@
 package com.example.playlistmaster
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
-import android.widget.ImageView
+
 
 class SettingsActivity : AppCompatActivity() {
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
