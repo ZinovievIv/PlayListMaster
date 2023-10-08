6c41d86799cb1900fe4202539cbbe20e695fdef0
zinoviev <zinovev.ivan@mail.ru>
2023-10-08 23:23:47 +0300

    Застрял на отправке емейл

--- app/src/main/java/com/example/playlistmaster/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaster/SettingsActivity.kt
@@ -1,27 +1,46 @@
 package com.example.playlistmaster
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val arrowBack = findViewById<ImageView>(R.id.arrow_back)
+        val buttonArrowBack = findViewById<ImageView>(R.id.arrow_back)
+        val buttonShare = findViewById<ImageView>(R.id.button_share)
+        val buttonSupport = findViewById<ImageView>(R.id.button_support)
 
         val imageButtonClickListener: View.OnClickListener = View.OnClickListener { v ->
             when (v?.id) {
                 R.id.arrow_back -> {
-                        val arrowBackIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-                        startActivity(arrowBackIntent)
+                    val arrowBackIntent = Intent(this@SettingsActivity, MainActivity::class.java)
+                    startActivity(arrowBackIntent)
+                }
+                R.id.button_share -> {
+                    val browseIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://practicum.yandex.ru/android-developer/")
+                    )
+                    startActivity(browseIntent)
+                }
+                R.id.button_support -> {
+                    val message = "Привет, Android-разработка — это круто!"
+                    val shareIntent = Intent(Intent.ACTION_SENDTO)
+                    shareIntent.data = Uri.parse("mailto:zinovev.ivan@mail.ru")
+                    shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("zinovev.ivan@mail.ru"))
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+                    startActivity(shareIntent)
                 }
             }
         }
-        arrowBack.setOnClickListener(imageButtonClickListener)
+        buttonShare.setOnClickListener(imageButtonClickListener)
+        buttonArrowBack.setOnClickListener(imageButtonClickListener)
+        buttonSupport.setOnClickListener(imageButtonClickListener)
     }
 }
