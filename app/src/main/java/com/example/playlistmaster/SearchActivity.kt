89a24e78a770ad33def2f342c45fbef8dc22d27b
Зиновьев Иван <zinovev.ivan@mail.ru>
2023-10-17 19:46:24 +0300

    Попытка настроить сохранение при смене активити

--- app/src/main/java/com/example/playlistmaster/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaster/SearchActivity.kt
@@ -1,11 +1,58 @@
 package com.example.playlistmaster
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class SearchActivity : AppCompatActivity() {
+
+    var text = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val arrow_Back = findViewById<ImageView>(R.id.arrow_back)
+        val searchBar = findViewById<EditText>(R.id.searchBar)
+
+
+        arrow_Back.setOnClickListener {
+            val intent = Intent(this@SearchActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                text = p0.toString()
+            }
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+        searchBar.addTextChangedListener(textWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(PRODUCT_AMOUNT, text)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchBar = findViewById<EditText>(R.id.searchBar)
+        searchBar.setText(savedInstanceState.getString(PRODUCT_AMOUNT))
+    }
+
+companion object {
+    const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
+    const val AMOUNT = ""
+}
 }
