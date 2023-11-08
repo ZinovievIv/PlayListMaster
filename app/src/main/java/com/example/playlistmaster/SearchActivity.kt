95663022d82266ddb3f5cc86064ae191bd005729
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-11-08 19:10:35 +0300

    Заработала сеть, получает запросы

--- app/src/main/java/com/example/playlistmaster/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaster/SearchActivity.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -26,18 +32,13 @@ class SearchActivity : AppCompatActivity() {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     private val itunesService = retrofit.create(ItunesApi::class.java)
+    private val adapter = TracksAdapter(trackList, this)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkTheme()
         setContentView(R.layout.activity_search)
-
-        trackList.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01","https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
-        trackList.add(Track("Billie Jean", "Michael Jackson", "4:35","https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
-        trackList.add(Track("Stayin' Alive", "Bee Gees", "4:10","https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
-        trackList.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33","https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
-        trackList.add(Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
 
         val recycle = findViewById<RecyclerView>(R.id.recycleView)
         recycle.layoutManager = LinearLayoutManager(this)
@@ -49,9 +50,43 @@ class SearchActivity : AppCompatActivity() {
         val clearButton = findViewById<ImageView>(R.id.clearButton)
 
         clearButton.setOnClickListener {
-            searchBar.setText("")
-            searchBar.hideKeyboard()
+            if (searchBar.text.isNotEmpty()) {
+                itunesService.findTrack(searchBar.text.toString())
+                    .enqueue(object : Callback<TrackResponse> {
+                        override fun onResponse(
+                            call: Call<TrackResponse>,
+                            response: Response<TrackResponse>
+                        ) {
+                            if (response.code() == 200) {
+                                trackList.clear()
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    trackList.addAll(response.body()?.results!!)
+                                    Log.i("Test", "${trackList.toString()}")
+                                    adapter.notifyDataSetChanged()
+                                    recycle.visibility = View.VISIBLE
+                                }
+                                if (trackList.isEmpty()) {
+                                    showMessage("", "")
+                                } else {
+                                    showMessage("", "")
+                                }
+                            } else {
+                                showMessage("", "")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+            }
         }
+
+
+            //clearButton.setOnClickListener {
+            // searchBar.setText("")
+            //searchBar.hideKeyboard()
+        //}
 
         arrowBack.setOnClickListener {
             finish()
@@ -84,6 +119,21 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         val searchBar = findViewById<EditText>(R.id.searchBar)
         searchBar.setText(savedInstanceState.getString(PRODUCT_AMOUNT))
+    }
+
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            //placeholderMessage.visibility = View.VISIBLE
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            //placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            //placeholderMessage.visibility = View.GONE
+        }
     }
 
     fun View.hideKeyboard() {
