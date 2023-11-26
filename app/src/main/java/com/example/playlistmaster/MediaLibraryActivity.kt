e21e07f1d4eefe1a9fc1a9ab9808197350481755
zinoviev <zinovev.ivan@mail.ru>
2023-11-26 19:01:23 +0300

    Отрисован Экран плеера

--- app/src/main/java/com/example/playlistmaster/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaster/MediaLibraryActivity.kt
@@ -1,13 +1,22 @@
 package com.example.playlistmaster
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.playlistmaster.databinding.ActivityMediaLibraryBinding
 
 class MediaLibraryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMediaLibraryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_library)
+        binding = ActivityMediaLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbarMediaLibrary.setOnClickListener{
+            finish()
+        }
     }
+
 
 }
